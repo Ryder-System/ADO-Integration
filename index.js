941405705b0d0cbc70fdd1a2bbb125bf11fd0e7e
Kuriakose Thomas <103944398+kuriakose-thomas@users.noreply.github.com>
2022-08-15 10:23:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -731,7 +731,7 @@ function getValuesFromPayload(payload, env) {
 
     // update title of the issue if there is a label with name 'qa bug'
     if(vm.labels.includes('qa bug')){
-        vm.title = `${vm.title.replace(" - [Production Support]", "").replace("[Production Support]", "")} - [Production Support]`;
+        vm.title = `${vm.title.replace(" - [Prod Support]", "").replace("[Prod Support]", "")} - [Prod Support]`;
     }
 
     // comments are not always part of the payload
